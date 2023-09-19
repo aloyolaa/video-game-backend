a8218427f0f4047cc969cec4823657d5c6673222
Alan Loyola <alanloyola2002@gmail.com>
2023-09-19 15:40:05 -0500

    feat: Dependencias agregadas y Conectividad a Base de Datos

---
+++ src/main/java/com/aloyolaa/videogamebackend/VideoGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aloyolaa.videogamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aloyolaa/videogamebackend/VideoGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aloyolaa.videogamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
